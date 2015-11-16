fc1cc62942efadf5027cd85febe8e12c99bf78cb
Pauls Bebris <pauls.bebris@gmail.com>
2015-11-16 13:30:20 +0200

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
+    private static int randomNumber = 0;
+    private static int userGuess = 0;
 
 
     public static void main(String[] args) {
@@ -19,22 +20,34 @@ public class Main {
         System.out.println("Computer generated  number from 1 to 100");
         Random number = new Random();
         int randomNumber = number.nextInt(100);
-//        System.out.println(randomNumber);
+        System.out.println(randomNumber);
             //correction for negative numbers
             if(randomNumber < 0){
-                randomNumber = randomNumber * (-1);
+                Main.randomNumber =  -1 * randomNumber;
+            } else {
+                Main.randomNumber = randomNumber;
             }
-        return randomNumber;
+        return Main.randomNumber;
     }
     // user guesses the number
     public static int userGuessesNumber(){
         System.out.println("Guess and enter number from 1 to 100");
         Scanner input = new Scanner(System.in);
         int userGuess = input.nextInt();
-        if(){
+//        Main.userGuess = userGuess;
 
+        if(userGuess == Main.randomNumber){
+            System.out.println("You guessed CORRECT");
+            Main.userGuess = userGuess;
+        } if (userGuess < Main.randomNumber){
+            System.out.println("Your number: " + userGuess + " is less that one computer generated");
+            Main.userGuess = userGuess;
+        } if (userGuess > Main.randomNumber) {
+            System.out.println("Your number: " + userGuess + " is bigger that one computer generated");
+            Main.userGuess = userGuess;
         }
-        System.out.println(userGuess);
-        return userGuess;
+//        System.out.println(userGuess);
+
+        return Main.userGuess;
     }
 }
