5bb4d58d8a7de54807b5aff259b370030503f956
Pauls Bebris <pauls.bebris@gmail.com>
2015-11-16 12:12:40 +0200

    nearly ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+
+    computerGeneratesNumber();
+    userGuessesNumber();
+
+    }
+
+    // generate random number from 1 to 100
+    public static int computerGeneratesNumber(){
+        System.out.println("Computer generated  number from 1 to 100");
+        Random number = new Random();
+        int randomNumber = number.nextInt(100);
+//        System.out.println(randomNumber);
+            //correction for negative numbers
+            if(randomNumber < 0){
+                randomNumber = randomNumber * (-1);
+            }
+        return randomNumber;
+    }
+    // user guesses the number
+    public static int userGuessesNumber(){
+        System.out.println("Guess and enter number from 1 to 100");
+        Scanner input = new Scanner(System.in);
+        int userGuess = input.nextInt();
+        if(){
+
+        }
+        System.out.println(userGuess);
+        return userGuess;
     }
 }
